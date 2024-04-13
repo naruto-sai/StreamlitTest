1ca26890af2fb8cc2904fa9e1c32ace1dd243113
naruto-sai <vivekchowdarysai456@gmail.com>
2024-04-13 16:52:26 -0400

    newtrain model

--- app.py
+++ app.py
@@ -209,8 +209,12 @@ if st.button("Estimate Carbin Footprint"):
     else:
         r_metal=0
 
-    dict1 = {'GC_PM':grocery_bill, 'DT_PM':distance_travelled, 'TPW':tv_pc,
-       'NCP':new_clothes, 'IT':internet, 'BT_obese':bt_obese, 'BT_overweight':bt_ow, 'BT_underweight':bt_uw, 'S':sex,
+    # "Grocery_CostPM", "Dist_TravelledPM", "TV_PC_WatchtimePD", "New_Clothes_PM", "Internet_TimeH"
+    # [1]  173.36287 2042.42381   12.13766   25.05342   11.88657
+# [1]   72.386055 2777.828915    7.139277   14.692149    7.288290
+
+    dict1 = {'GC_PM':(grocery_bill-173.36287)/72.386055, 'DT_PM':(distance_travelled-2042.42381)/2777.828915, 'TPW':(tv_pc-12.13766)/7.139277,
+       'NCP':(new_clothes-25.05342)/14.692149, 'IT':(internet-11.88657)/7.288290, 'BT_obese':bt_obese, 'BT_overweight':bt_ow, 'BT_underweight':bt_uw, 'S':sex,
         'D_pescatarian':d_pesc, 'D_vegan':d_vegan, 'D_vegetarian':d_veg, 'SF_less_frequent':sf_lf, 'SF_more_frequent':sf_mf,
        'SF_twice_a_day':sf_td,  'HS_electric':h_elec, 'HS_gas':h_gas, 'HS_wood':_h_wood, 'T_public':trs_pu, 'T_walk_bike':trs_wa, 
        'VT_electric':vt_ele, 'VT_hybrid':vt_hyb, 'VT_lpg':vt_lpg, 'VT_petrol':vt_pet, 'VT_PMM':vt_pmm, 'VT_public':vt_pub, 
